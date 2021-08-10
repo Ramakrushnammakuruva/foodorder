0b688b03cde99098871cdb16837e02700e777a22
Ramakrushnammakuruva <88617791+Ramakrushnammakuruva@users.noreply.github.com>
2021-08-10 14:44:36 +0530

    tikka pattinde

--- src/main/java/com/eample/foodorder/FoodorderApplication.java
+++ src/main/java/com/eample/foodorder/FoodorderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodorderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FoodorderApplication.class, args);
+        SpringApplication.run(FoodorderApplication.class, args);//ky
     }
 
 }
